1c68973dfa4f94aa887012e2fbd1e883bea5b5d6
wegdesign <rcommodaro@gmail.com>
2015-10-13 14:06:27 +0200

    impostazioni tema

--- functions.php
+++ functions.php
@@ -12,5 +12,21 @@
   */
   
   	require_once( 'inc/impostazioni_tema.php');
-
+	
+	// Rendo il tema disponibile alle traduzioni
+	// Le traduzioni potranno essere trovate dentro la cartella /languages/
+	function localizzo_tema(){
+	
+		load_theme_textdomain( 'templatezero', TEMPLATEPATH . '/languages' );
+	
+		$locale = get_locale();
+	
+		$locale_file = TEMPLATEPATH . "/languages/$locale.php";
+	
+		if ( is_readable($locale_file) ) require_once($locale_file);
+	}
+	
+	add_action( 'after_setup_theme', 'localizzo_tema' );
+	
+	
 ?>
